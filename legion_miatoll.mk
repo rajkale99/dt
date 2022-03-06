e92d6702c53507c5859a49eddf1b7621dd7cd873
rajkale99 <kaleraj.rk@gmail.com>
2022-03-06 20:40:52 +0530

    device:Bringup LegionOS

--- legion_miatoll.mk
+++ legion_miatoll.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The ArrowOS
+# Copyright (C) 2020 The LegionOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -7,15 +7,15 @@
 # Inherit from miatoll  device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common LegionOS stuff
+$(call inherit-product, vendor/legion/config/common.mk)
 $(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := arrow_miatoll
+PRODUCT_NAME := legion_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Miatoll
@@ -24,7 +24,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Maintainer
-DEVICE_MAINTAINER := SonalSingh18
+DEVICE_MAINTAINER := Rajkale99
+LEGION_OFFICIAL := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
